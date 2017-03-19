6d6e9c053fa560ab6ce56450ce1d5b8d040a6b3b
DrakeW <junyuw@berkeley.edu>
2017-03-18 21:48:42 -0700

    make use of closure

--- models/indexer.go
+++ models/indexer.go
@@ -27,7 +27,7 @@ func statTimes(fi os.FileInfo) (atime, mtime, ctime time.Time, err error) {
 func StartIndexing(dirName string, fr_index bleve.Index, doneChan chan bool) {
 	quitChan := make(chan bool)
 	frChan := make(chan *FileRecord)
-	go func(fr_index bleve.Index, frChan chan *FileRecord, quitChan chan bool, doneChan chan bool) {
+	go func(fr_index bleve.Index) {
 		batch := fr_index.NewBatch()
 		batchCount := 0
 		for {
@@ -57,7 +57,7 @@ func StartIndexing(dirName string, fr_index bleve.Index, doneChan chan bool) {
 				}
 			}
 		}
-	}(fr_index, frChan, quitChan, doneChan)
+	}(fr_index)
 	IndexAllFiles(dirName, frChan)
 	quitChan <- true
 }
