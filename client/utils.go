3b1bffbea84af1372de9cb96bff403a471756ecb
DrakeW <junyuw@berkeley.edu>
2017-03-20 19:28:29 -0700

    colorized output

--- client/utils.go
+++ client/utils.go
@@ -4,21 +4,24 @@ import (
 	"fmt"
 	"github.com/blevesearch/bleve"
 	"github.com/gosuri/uitable"
+	"github.com/logrusorgru/aurora"
 )
 
 func formatSearchResult(result *bleve.SearchResult) string {
 	table := uitable.New()
-	table.MaxColWidth = 80
+	table.MaxColWidth = 120
 	table.Wrap = true
-	summary := fmt.Sprintf("%d matches, showing %d through %d, took %s\n", result.Total, result.Request.From+1, result.Request.From+len(result.Hits), result.Took)
+	summary := aurora.Brown(fmt.Sprintf("%d matches, showing %d through %d, took %s\n", result.Total, result.Request.From+1, result.Request.From+len(result.Hits), result.Took))
 	table.AddRow("")
 	for _, hit := range result.Hits {
 		path := hit.ID
-		table.AddRow("Path:", path)
+		table.AddRow(aurora.Green("Path:"), path)
 		for field, v := range hit.Fragments {
-			table.AddRow(fmt.Sprintf("%s:", field), v)
+			if field != "Path" && field != "Extension" {
+				table.AddRow(fmt.Sprintf("%s:", aurora.Green(field)), fmt.Sprintf("...%s...", v))
+			}
 		}
 		table.AddRow("") // blank
 	}
-	return summary + table.String()
+	return summary.String() + table.String()
 }
