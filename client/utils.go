380764409224451e1280d8d93338627f82af6ce2
DrakeW <junyuw@berkeley.edu>
2017-03-20 17:20:52 -0700

    separated results

--- client/utils.go
+++ client/utils.go
@@ -6,16 +6,19 @@ import (
 	"github.com/gosuri/uitable"
 )
 
-func formatSearchResult(result *bleve.SearchResult) *uitable.Table {
+func formatSearchResult(result *bleve.SearchResult) string {
 	table := uitable.New()
 	table.MaxColWidth = 80
 	table.Wrap = true
+	summary := fmt.Sprintf("%d matches, showing %d through %d, took %s\n", result.Total, result.Request.From+1, result.Request.From+len(result.Hits), result.Took)
+	table.AddRow("")
 	for _, hit := range result.Hits {
 		path := hit.ID
 		table.AddRow("Path:", path)
 		for field, v := range hit.Fragments {
 			table.AddRow(fmt.Sprintf("%s:", field), v)
 		}
+		table.AddRow("") // blank
 	}
-	return table
+	return summary + table.String()
 }
